478336d372c3bbc2d300e4dd80b0315c57a863eb
st8ingenious <29981v4>
2015-05-26 20:26:40 +0300

    check if timeout.exe is running properly

--- progcheckbyst8/main.bat
+++ progcheckbyst8/main.bat
@@ -8,7 +8,6 @@ set Today=%Date:~4,2%/%Date:~7,2%/%Date:~10,4%
 set _time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
 echo Program Status Check By St8 Started %_time% %Today% >> C:\progcheckbyst8\log.txt
 set /a xxxx=0
-set /a ffirst=0
 :loop
 tasklist /FI "IMAGENAME eq %checkexe%" 2>NUL | find /I /N "%checkexe%" >NUL
 if "%ERRORLEVEL%"=="0" (
@@ -21,15 +20,21 @@ set /a xxxx=0
 )
 set /a xxxx+=1
 ) else (
-echo Program STOPPED. 
+tasklist /FI "IMAGENAME eq timeout.exe" 2>NUL | find /I /N "timeout.exe" >NUL
+if "%ERRORLEVEL%"=="0" (
 set Today=%Date:~4,2%/%Date:~7,2%/%Date:~10,4%
 set _time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+echo Program STOPPED. 
 echo ######################################################################################## >> C:\progcheckbyst8\log.txt
 echo ALERT Program stopped !_time! !Today! >> C:\progcheckbyst8\log.txt
 echo ######################################################################################## >> C:\progcheckbyst8\log.txt
 start /min C:\progcheckbyst8\alert.mp3
 start C:\progcheckbyst8\alert.txt
 msg "%username%" ALERT!!!! PROGRAM STOPPED
+) else (
+msg "%username%" ALERT!!!! Progcheck ERROR or Windows shutdown taking too long.
+start C:\progcheckbyst8\alert.txt
+)
 )
 timeout /t 120 /NOBREAK
 goto :loop
